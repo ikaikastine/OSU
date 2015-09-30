b04764c2a95238ff51a7cb46b830c3a2705cb50d
Kevin Stine <ikaikastine@gmail.com>
2015-09-29 18:27:10 -0700

    Added command line options for rows/cols

--- cs344/assignments/stats/stats.sh
+++ cs344/assignments/stats/stats.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo This will calculate your stats
+	if [ "$1" = "-r" ]; then
+		echo Will calculate rows
+	elif [ "$1" = "-c" ]; then
+		echo will calculate columns
+	else 
+		echo Invalid input
+	fi
+
