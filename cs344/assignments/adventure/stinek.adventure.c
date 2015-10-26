f90b4b68007bca7d0ca4e1b747abfda2c69e4fac
Kevin Stine <ikaikastine@gmail.com>
2015-10-26 14:22:00 -0700

    Implementing function which creates 7 files

--- cs344/assignments/adventure/stinek.adventure.c
+++ cs344/assignments/adventure/stinek.adventure.c
@@ -7,7 +7,10 @@
 struct stat st = {0};
 
 int main () {
-	FILE *fp;
+	int numFiles = 7;
+	FILE *files[numFiles];
+	char filename[20];
+  int i;
 
 	int pid = getpid();
 	printf("PID: %d\n", pid);
@@ -15,7 +18,12 @@ int main () {
 	//Create initial directory for files to be stored in
 	mkdir("stinek.adventure.$$", 0777);	
 	//Create first file
-	fp = fopen("/stinek.adventure.$$/
-	 
+	
+	for (i = 0; i < numFiles; i++) {
+		sprintf(filename, "stinek.adventure.$$/file%d.c", i);
+		files[i] = fopen (filename, "w");
+	}
+	//fp = fopen("/stinek.adventure.$$/
+
 	return 0;
 }
