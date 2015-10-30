023ab88bbe4121fe7d0d7b36b3625b1a82dbeaeb
Kevin Stine <ikaikastine@gmail.com>
2015-10-29 17:15:04 -0700

    Updated to create rooms within directory

--- cs344/assignments/adventure/stinek.adventure.c
+++ cs344/assignments/adventure/stinek.adventure.c
@@ -9,9 +9,44 @@
 
 struct Positions {
 	char *start;
-	char *mid;
+	char *connection;
 	char *end;
 };
+
+struct Positions createRooms(char *directory) {
+	struct Positions location;
+	location.connection = directory;
+
+	char *room[10];
+	room[0] = "Barracks";
+	room[1] = "Prison";
+	room[2] = "Library";
+	room[3] = "Shrine";
+	room[4] = "Crypt";
+	room[5] = "Guardhouse";
+	room[6] = "Laboratory";
+	room[7] = "Armory";
+	room[8] = "Bestiary";
+	room[9] = "Garden";
+	int bufferSize = 128;
+	char *currentFile = malloc(128);
+	int i, j;
+
+	for (i = 0; i < 7; i++) {
+		snprintf(currentFile, bufferSize, "%s/%s", directory, room[i]);
+		FILE *fp = fopen(currentFile, "w");
+		if (fp == NULL) {
+			perror ("Error opening file.");
+		}
+		else {
+			fprintf(fp, "Room Name: %s\n", room[i]);
+		}
+		fclose(fp);
+	}
+
+
+
+}
 
 char *createDirectory(int pID) {
 	char *directory = malloc(20);
@@ -28,15 +63,12 @@ char *createDirectory(int pID) {
 
 
 int main () {
-	int numFiles = 7;
-	FILE *files[numFiles];
-	char filename[20];
-  int i;
-
 	int pid = getpid();
 	printf("PID: %d\n", pid);
 
-	createDirectory(pid);	
+	char *roomsDirectory = createDirectory(pid);	
 	
+	createRooms(roomsDirectory);
+
 	return 0;
 }
