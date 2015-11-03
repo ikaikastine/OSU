f265728c080c56951420abb8b1b4a34ac1e2ed89
Kevin Stine <ikaikastine@gmail.com>
2015-11-02 22:00:45 -0800

    Finished implementation. Still bugs to squash.

--- cs344/assignments/adventure/stinek.adventure.c
+++ cs344/assignments/adventure/stinek.adventure.c
@@ -14,6 +14,22 @@ struct Positions
 	char *end;
 };
 
+void shuffle(char **array, size_t n);
+
+char *createDirectory(int pID) 
+{
+	char *directory = malloc(20);
+	char *dirName = "stinek.adventure.";
+
+	sprintf(directory, "%s%d", dirName, pID);
+
+	struct stat st = {0};
+	if (stat(directory, &st) == -1) {
+		mkdir(directory, 0755);
+	}
+	return directory; 
+}
+
 struct Positions createRooms(char *directory) 
 {
 	struct Positions location;
@@ -32,7 +48,7 @@ struct Positions createRooms(char *directory)
 	room[9] = "Garden";
 	char *currentFile = malloc(128);
 	int i, j;
-
+	shuffle(room, 10);
 	for (i = 0; i < 7; i++) {
 		sprintf(currentFile, "%s/%s", directory, room[i]);
 		FILE *fp = fopen(currentFile, "w");
@@ -88,29 +104,124 @@ struct Positions createRooms(char *directory)
 }
 
 
-char *createDirectory(int pID) 
+void beginAdventure(struct Positions location) 
 {
-	char *directory = malloc(20);
-	char *dirName = "stinek.adventure.";
+	char *currentRoom = location.start;
+	char *finalRoom = location.end;
+	char *directory = location.connection;
+	int stepCount = 0;
+	int i, valid, c, connectionNum;
+	char (*steps)[15] = malloc(sizeof *steps * 8);
+	char (*contents)[15] = malloc(sizeof *contents * 8);
+	char destination[15];
 
-	sprintf(directory, "%s%d", dirName, pID);
+	char *currentFile = malloc(128);
+	printf("seg fault at first strcmp");
+	while (!(strcmp(currentRoom, finalRoom)) == 0) {
+		printf("seg fault after first strcmp");
+		sprintf(currentFile, "%s/%s", directory, currentRoom);
+		FILE *fp = fopen(currentFile, "r");
 
-	struct stat st = {0};
-	if (stat(directory, &st) == -1) {
-		mkdir(directory, 0755);
+		connectionNum = 0;
+
+		if (fp) {
+			while ((c = getc(fp)) != EOF) {
+				if (c == '\n') {
+					connectionNum++;
+				}
+			}
+			connectionNum = connectionNum - 2;
+
+			char str[20];
+			fseek(fp, 11, SEEK_SET);
+			fgets(str, 20, fp);
+			int len = strlen(str);
+			if(str[len-1] == '\n') {
+				str[len-1] = 0;
+			}
+			strcpy(contents[0], str);
+
+			for (i = 1; i <= connectionNum; i++) {
+				fseek(fp, 14, SEEK_CUR);
+				fgets(str, 20, fp);
+				len = strlen(str);
+				if(str[len-1] == '\n') {
+					str[len-1] = 0;
+				}
+				strcpy(contents[i], str);
+			}
+			valid = 0;
+			while (valid == 0) {
+				printf("CURRENT LOCATION: %s\n", contents[0]);
+				printf("POSSIBLE CONNECTIONS: ");
+				for (i = 1; i <= connectionNum; i++) {
+					if (i == connectionNum) {
+						printf("%s.\n", contents[i]);
+					}
+					else {
+						printf("%s, ", contents[i]);
+					}
+				}
+				printf("WHERE TO? > ");
+				scanf("%s", destination);
+				for (i = 1; i <= connectionNum; i++) {
+					if (strcmp(destination, contents[i]) == 0) {
+						valid = 1;
+						currentRoom = destination;
+					}
+				}
+				if (valid != 1) {
+					printf("\nHUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
+				}
+			}
+			printf("\n");
+			strcpy(steps[stepCount], currentRoom);
+			stepCount++;
+			fclose(fp);
+		}
+		printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!");
+		printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", stepCount);
+		for (i = 0; i < stepCount; i++) {
+			printf("%s\n", steps[i]);
+		}
 	}
-	return directory; 
+	free(steps);
+	free(contents);
+	free (currentFile);
 }
 
+void shuffle(char **array, size_t n)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	int usec = tv.tv_usec;
+	srand48(usec);
+
+	if (n > 1) {
+        size_t i;
+        for (i = n - 1; i > 0; i--) {
+            size_t j = (unsigned int) (drand48()*(i+1));
+            char* t = array[j];
+            array[j] = array[i];
+            array[i] = t;
+        }
+    }
+}
 
 int main ()
 {
+	srand(time(NULL));
+
 	int pid = getpid();
-	printf("PID: %d\n", pid);
 
 	char *roomsDirectory = createDirectory(pid);	
-	
-	createRooms(roomsDirectory);
+	printf("makes it out of create directory");
+	struct Positions location = createRooms(roomsDirectory);
+
+	printf("printf before begin adventure");
+	beginAdventure(location);
+	printf("printf after begin adventure");
+	free(roomsDirectory);
 
 	return 0;
 }
