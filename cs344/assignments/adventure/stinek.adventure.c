ab8697481aa4f44ab12a7f229fbbc49f407ffd50
Kevin Stine <ikaikastine@gmail.com>
2015-11-04 13:37:08 -0800

    Fixed a few bugs

--- cs344/assignments/adventure/stinek.adventure.c
+++ cs344/assignments/adventure/stinek.adventure.c
@@ -14,12 +14,13 @@ struct Positions
 	char *end;
 };
 
-char *createDirectory(int pID) 
+char *createDirectory() 
 {
+	int pid = getpid();
 	char *directory = malloc(20);
 	char *dirName = "stinek.adventure.";
 
-	sprintf(directory, "%s%d", dirName, pID);
+	sprintf(directory, "%s%d", dirName, pid);
 
 	struct stat st = {0};
 	if (stat(directory, &st) == -1) {
@@ -47,6 +48,7 @@ struct Positions createRooms(char *directory)
 	char *currentFile = malloc(128);
 	int i, j, k;
 
+	//Swaps files around so they aren't the same each game
 	for (k = 0; k < 9; k++) {
 		int swap = rand() % 7;
 		char* temp = room[swap];
@@ -71,7 +73,7 @@ struct Positions createRooms(char *directory)
 	int initPos = rand() % 7;
 	int endPos = rand() % 7;
 	char *connection;
-	int connectionNum, idx;
+	int connections, idx;
 	char currentRoom[100];
 
 	for(i = 0; i < 7; i++) {
@@ -82,9 +84,9 @@ struct Positions createRooms(char *directory)
 			perror ("Error opening file.");
 		}
 		else {
-			connectionNum = rand() % 4 + 3;
+			connections = rand() % 4 + 3;
 			idx = 0;
-			for (j = 0; j < connectionNum; j++) {
+			for (j = 0; j < connections; j++) {
 				connection = room[idx];
 				if (connection == room[i]) {
 					idx++;
@@ -118,7 +120,7 @@ void beginAdventure(struct Positions location)
 	char *finalRoom = location.end;
 	char *directory = location.connection;
 	int stepCount = 0;
-	int i, valid, c, connectionNum;
+	int i, valid, c, connections;
 	char (*steps)[15] = malloc(sizeof *steps * 8);
 	char (*contents)[15] = malloc(sizeof *contents * 8);
 	char destination[15];
@@ -130,15 +132,15 @@ void beginAdventure(struct Positions location)
 		sprintf(currentFile, "%s/%s", directory, currentRoom);
 		FILE *fp = fopen(currentFile, "r");
 
-		connectionNum = 0;
+		connections = 0;
 
 		if (fp) {
 			while ((c = getc(fp)) != EOF) {
 				if (c == '\n') {
-					connectionNum++;
+					connections++;
 				}
 			}
-			connectionNum = connectionNum - 2;
+			connections = connections - 2;
 
 			char str[20];
 			fseek(fp, 11, SEEK_SET);
@@ -149,7 +151,7 @@ void beginAdventure(struct Positions location)
 			}
 			strcpy(contents[0], str);
 
-			for (i = 1; i <= connectionNum; i++) {
+			for (i = 1; i <= connections; i++) {
 				fseek(fp, 14, SEEK_CUR);
 				fgets(str, 20, fp);
 				len = strlen(str);
@@ -162,8 +164,8 @@ void beginAdventure(struct Positions location)
 			while (valid == 0) {
 				printf("CURRENT LOCATION: %s\n", contents[0]);
 				printf("POSSIBLE CONNECTIONS: ");
-				for (i = 1; i <= connectionNum; i++) {
-					if (i == connectionNum) {
+				for (i = 1; i <= connections; i++) {
+					if (i == connections) {
 						printf("%s.\n", contents[i]);
 					}
 					else {
@@ -172,7 +174,7 @@ void beginAdventure(struct Positions location)
 				}
 				printf("WHERE TO? > ");
 				scanf("%s", destination);
-				for (i = 1; i <= connectionNum; i++) {
+				for (i = 1; i <= connections; i++) {
 					if (strcmp(destination, contents[i]) == 0) {
 						valid = 1;
 						currentRoom = destination;
@@ -184,16 +186,14 @@ void beginAdventure(struct Positions location)
 			}
 		}
 		printf("\n");
-			strcpy(steps[stepCount], currentRoom);
-			stepCount++;
-			fclose(fp);
-
-		
+		strcpy(steps[stepCount], currentRoom);
+		stepCount++;
+		fclose(fp);
 		}
 	printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!");
-		printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", stepCount);
-		for (i = 0; i < stepCount; i++) {
-			printf("%s\n", steps[i]);
+	printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", stepCount);
+	for (i = 0; i < stepCount; i++) {
+		printf("%s\n", steps[i]);
 	}
 	free(steps);
 	free(contents);
@@ -204,13 +204,10 @@ int main ()
 {
 	srand(time(NULL));
 
-	int pid = getpid();
-
-	char *roomsDirectory = createDirectory(pid);	
+	char *roomsDirectory = createDirectory();	
 	struct Positions location = createRooms(roomsDirectory);
 
 	beginAdventure(location);
-	free(roomsDirectory);
 
 	return 0;
 }
