cd4f804125fd442afa70dde6126bd1f39a1a2f69
Kevin Stine <ikaikastine@gmail.com>
2015-11-10 09:51:45 -0800

    Updated headers

--- cs344/assignments/smallsh/smallsh.c
+++ cs344/assignments/smallsh/smallsh.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <singla.h>
+
 
 int main () {
 
