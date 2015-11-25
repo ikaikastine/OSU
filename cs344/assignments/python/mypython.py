316fb921b783c32fc265ac17bac546dbb616b27c
Kevin Stine <ikaikastine@gmail.com>
2015-11-25 09:49:55 -0800

    Implemented random letters writing to file

--- cs344/assignments/python/mypython.py
+++ cs344/assignments/python/mypython.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
 
 import sys
+import random
+import string
+
+string.letters = 'abcdefghijklmnopqrstuvwxyz'
 
 file = open('file1.txt', 'w+')
-file.write("My first text file!\n");
+for x in range(0, 10):
+	file.write(random.choice(string.lowercase));
+
+line = file.read(10)
+file.write('\n');
+print "%s" % (line)
 #print (random.choice(string.lowercase) for x in range(10))
 file.close()
