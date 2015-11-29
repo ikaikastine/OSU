ff0210558b2caec2089d441eca41daeef0c5b124
Kevin Stine <ikaikastine@gmail.com>
2015-11-28 17:18:30 -0800

    Implemented the files to be written and read

--- cs344/assignments/python/mypython.py
+++ cs344/assignments/python/mypython.py
@@ -4,14 +4,35 @@ import sys
 import random
 import string
 
-string.letters = 'abcdefghijklmnopqrstuvwxyz'
-
+#Write random string to file 1
 file = open('file1.txt', 'w+')
 for x in range(0, 10):
 	file.write(random.choice(string.lowercase));
+file.close()
+#Read string from file 1
+file = open('file1.txt', 'r')
+line = file.readline(10)
+print "File 1 contains: %s" % (line)
+file.close()
 
-line = file.read(10)
-file.write('\n');
-print "%s" % (line)
-#print (random.choice(string.lowercase) for x in range(10))
+#Write random string to file 2
+file = open('file2.txt', 'w+')
+for x in range(0, 10):
+	file.write(random.choice(string.lowercase));
+file.close()
+#Read string from file 2
+file = open('file2.txt', 'r')
+line = file.readline(10)
+print "File 2 contains: %s" % (line)
+file.close()
+
+#Write random string to file 3
+file = open('file3.txt', 'w+')
+for x in range(0, 10):
+	file.write(random.choice(string.lowercase));
+file.close()
+#Read string from file 3
+file = open('file3.txt', 'r')
+line = file.readline(10)
+print "File 3 contains: %s" % (line)
 file.close()
