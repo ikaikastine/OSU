b8e968ad556399cdfdf03bb954a8ac833158be46
Kevin Stine <ikaikastine@gmail.com>
2015-11-30 18:47:34 -0800

    Initial implementation of the keygen

--- cs344/OTP/keygen.c
+++ cs344/OTP/keygen.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Usage: %s length\n", argv[0]);
+	}
+	else {
+		srand(time(NULL));
+		int i;
+		int len = atoi(argv[1]);
+		char curChar, key[len+1];
 
+		for (i = 0; i < len; i++) {
+			curChar = "ABCDEFGHIJKLMNOPQRSTUVQXYZ "[random() % 27];
+			key[i] = curChar;
+		}
+		key[len-1] = '\0';
+		printf("%s\n", key);
+	}
 
 	return 0;
 }
