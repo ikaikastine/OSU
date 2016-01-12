075c2ad7e62f219ea92805fc7f57efce5b5737e1
Kevin Stine <ikaikastine@gmail.com>
2016-01-12 11:22:44 -0800

    Added dynamic programming implementation. Removed maxSuffix

--- cs325/assignment_1/maxSubArray.py
+++ cs325/assignment_1/maxSubArray.py
@@ -27,19 +27,13 @@ def betterEnumeration(array):
 	print "Better enumeration =", maxSum
 	return maxSum
 
-def maxSuffix(array):
-	reverseArray = array[::-1]
-	max = reverseArray[0]
-	sum = 0
-	for i in reverseArray:
-		sum += i
-		if sum > max:
-			max = sum
-	print "Max suffix =", max
-	return max
-
-#def dynamicProgramming(array):
-
+def dynamicProgramming(array):
+	best = cur = 0
+	for i in array:
+		cur = max(cur + i, 0)
+		best = max(best, cur)
+	print "Dynamic programming =", best
+	return best
 
 
 #Call basic enumeration implementation
@@ -48,5 +42,5 @@ enumeration([31, -41, 59, 26, -53, 58, 97, -93, -23, 84])
 #Call better enumeration implementation
 betterEnumeration([31, -41, 59, 26, -53, 58, 97, -93, -23, 84])
 
-#Call maxSuffix
-maxSuffix([31, -41, 59, 26, -53, 58, 97, -93, -23, 84])
+#Call dynamic programming implementation 
+dynamicProgramming([31, -41, 59, 26, -53, 58, 97, -93, -23, 84])
