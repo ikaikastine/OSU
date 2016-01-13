53e2bb3c51a04deb850cc8e9178eab2204810cdf
ikaikastine <ikaikastine@gmail.com>
2016-01-12 21:07:36 -0800

    Implemented writing to file

--- cs325/assignment_1/maxSubArray.py
+++ cs325/assignment_1/maxSubArray.py
@@ -1,4 +1,4 @@
-import csv, random, sys, timeit
+import csv, random, sys, timeit, re
 
 #Basic max sub array implemetation 
 def enumeration(array):
@@ -9,6 +9,10 @@ def enumeration(array):
 			if newSum > maxSum:
 				maxSum = newSum
 	print "Enumeration =", maxSum
+	file = open('output.txt', 'w+')
+	print maxSum
+	file.write(str(maxSum))
+	file.write(' ')
 	return maxSum
 
 #Better max sub array implementation
@@ -25,6 +29,9 @@ def betterEnumeration(array):
 				maxSum = newSum
 			oldSum = newSum
 	print "Better enumeration =", maxSum
+	file = open('output.txt', 'a')
+	file.write(str(maxSum))
+	file.write(' ')
 	return maxSum
 
 def dynamicProgramming(array):
@@ -33,11 +40,17 @@ def dynamicProgramming(array):
 		cur = max(cur + i, 0)
 		best = max(best, cur)
 	print "Dynamic programming =", best
+	file = open('output.txt', 'a')
+	file.write(str(best))
+	file.write(' ')
 	return best
 
 
 #Call basic enumeration implementation
-enumeration([31, -41, 59, 26, -53, 58, 97, -93, -23, 84])
+file = open('testing.txt', 'r')
+line = file.readline()
+content = map(int, re.findall(r'\d+', line))
+enumeration(int(content))
 
 #Call better enumeration implementation
 betterEnumeration([31, -41, 59, 26, -53, 58, 97, -93, -23, 84])
