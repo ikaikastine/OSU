1f87ee58068c1a0f7687fb52ac3336785c889008
Kevin Stine <ikaikastine@gmail.com>
2016-01-20 13:26:38 -0800

    Added incorrect usage prompt

--- cs325/assignment_2/assign2.py
+++ cs325/assignment_2/assign2.py
@@ -1,1 +1,11 @@
 import os, random, time, sys, urllib2
+
+
+
+if __name__ == '__main__':
+	if not len (sys.argv) > 1:
+		print 'Usage: python assign2.py [option]'
+		print 'Options:'
+		options = ['--test', '--time <# of runs>']
+		print '\n'.join(map(lambda x:'\t' + x, options))
+		sys.exit()
